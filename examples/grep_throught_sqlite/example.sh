abb84545b23c8ded6fadad632b7dd614b8ce0963
jrx <jerome+git@rouaix.eu>
2024-03-04 11:45:28 +0100

    less expectations

--- examples/grep_throught_sqlite/example.sh
+++ examples/grep_throught_sqlite/example.sh
@@ -1,4 +1,4 @@
 
-scnr scan -i ../../_samples -f *.db | grep -B 1 -A 3 Islands > actual_output.txt
+scnr scan -i ../../_samples -f *.db | grep Islands > expected_output.txt
 
 diff expected_output.txt actual_output.txt
